ac0613cd5d45ffb8c494e267b4a4197f97b30cc7
William Johansson Visuri <williamvisuri@gmail.com>
2020-04-09 11:40:40 +0200

    Update extract_data.py

--- processing/extract_data.py
+++ processing/extract_data.py
@@ -1,12 +1,6 @@
-import math
 import numpy as np
 import os
-
-from PIL import Image
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from whitening import zca_whitening
-import matplotlib.pyplot as plt
-from tensorflow.keras.preprocessing.image import array_to_img
+import cv2
 
 def extract_data(path, channels):
     og_dir = os.listdir(path)
@@ -16,34 +10,24 @@ def extract_data(path, channels):
     masks = []
 
     for file_name in og_dir:
-        img = Image.open(path + file_name)
+        img = cv2.imread(path + file_name, 0)
         if 'image' in file_name:
             if channels != 1:
-                imgs.append(img.getdata())
+                imgs.append(img)
             else:
                 if (int(file_name[-7:-4]) % 7) == 3:
-                    imgs.append(img.getdata())
+                    imgs.append(img)
 
         elif 'mask' in file_name:
-            masks.append(img.getdata())
-    if np.shape(imgs)[1] // 256 == 256:
-        imgs_shape = 256
-    elif np.shape(imgs)[1] // 258 == 258:
-        imgs_shape = 258
-    else:
-        raise
-
-    imgs = np.array(imgs).reshape((-1, imgs_shape, imgs_shape, 1)).astype('float32')
-
+            masks.append(img)
+    imgs = np.array(imgs).reshape((-1, 258, 258, 1)).astype('float32')
     masks = np.array(masks).reshape((-1, 256, 256, 1))
-
     if channels > 1:
-        imgs = np.array(np.split(imgs, np.shape(imgs)[0]/7))
+        imgs = imgs.reshape(-1, 7, 258, 258, 1)
         imgs = np.swapaxes(imgs,1,2)
         imgs = np.swapaxes(imgs,2,3)
     if channels == 3:
         imgs = np.delete(imgs,[0,1,5,6], axis = 3)
     elif channels == 5:
         imgs = np.delete(imgs,[0,6], axis = 3)
-
     return imgs, masks
