f3ec51c9105bb8924e423fc6f546c31da3d76ac1
williamjvisuri <williamvisuri@gmail.com>
2020-04-17 16:47:01 +0200

    extract  more data

--- processing/split_grid.py
+++ processing/split_grid.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 from PIL import Image
 
@@ -9,10 +10,26 @@ def split_grid(images, masks, grid_split):
     img_split = []
     mask_split = []
 
-    for i in range(grid_split):
-        for j in range(grid_split):
-            img_split.append(images[:,img_shape*i:img_shape+2+img_shape*i,img_shape*j:img_shape+2+img_shape*j])
-            mask_split.append(masks[:,img_shape*i:img_shape+img_shape*i,img_shape*j:img_shape+img_shape*j])
+    n_patches = 30
+    rows = np.sort(random.sample(range(0, 192), n_patches))
+    cols = np.sort(random.sample(range(0, 192), n_patches))
+
+    for i,j in zip(rows, cols):
+        img_split.append(images[:,
+                                i:img_shape+2+i,
+                                j:img_shape+2+j])
+        mask_split.append(masks[:,
+                                i:img_shape+i,
+                                j:img_shape+j])
+
+    # for i in range(grid_split):
+    #     for j in range(grid_split):
+    #         img_split.append(images[:,
+    #                                 img_shape*i:img_shape+2+img_shape*i,
+    #                                 img_shape*j:img_shape+2+img_shape*j])
+    #         mask_split.append(masks[:,
+    #                                 img_shape*i:img_shape+img_shape*i,
+    #                                 img_shape*j:img_shape+img_shape*j])
 
     if np.shape(images)[3] > 1:
         img_split = np.array(img_split).reshape(-1,img_shape+2,img_shape+2,np.shape(images)[3],1)
@@ -23,5 +40,4 @@ def split_grid(images, masks, grid_split):
     # print('img shape: ', np.shape(img_split))
     # print('mask shape: ', np.shape(mask_split))
     # raise
-
     return img_split, mask_split
