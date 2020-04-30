f33fad8821d73bb1b342be0c860dbb7ce70fdce6
MikaelLennefors <miklen@student.chalmers.se>
2020-04-30 12:21:13 +0200

    png 384 files

---
+++ processing/extract_data_ratio.py
@@ -0,0 +1,34 @@
+import numpy as np
+import os
+import cv2
+
+def extract_data_ratio(path, channels, ratio):
+    og_dir = os.listdir(path)
+    og_dir.sort()
+
+    imgs = []
+    masks = []
+    ratio_name = 'image' + str(ratio)
+    mask_name = 'mask' + str(ratio)
+    for file_name in og_dir:
+        img = cv2.imread(path + file_name, 0)
+        if ratio_name in file_name:
+            if channels != 1:
+                imgs.append(img)
+            else:
+                if (int(file_name[-7:-4]) % 7) == 3:
+                    imgs.append(img)
+
+        elif mask_name in file_name:
+            masks.append(img)
+    imgs = np.array(imgs).reshape((-1, 258, 258, 1)).astype('float32')
+    masks = np.array(masks).reshape((-1, 256, 256, 1))
+    if channels > 1:
+        imgs = imgs.reshape(-1, 7, 258, 258, 1)
+        imgs = np.swapaxes(imgs,1,2)
+        imgs = np.swapaxes(imgs,2,3)
+    if channels == 3:
+        imgs = np.delete(imgs,[0,1,5,6], axis = 3)
+    elif channels == 5:
+        imgs = np.delete(imgs,[0,6], axis = 3)
+    return imgs, masks
---
+++ visualization/data_dist_ratio.py
@@ -0,0 +1,57 @@
+import sys
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy import stats
+
+sys.path.insert(1, '../processing')
+from extract_data_ratio import extract_data_ratio
+
+data_path = '../data/train_val_data_border_clean/'
+test_path = '../data/test_data_border_clean/'
+
+sns.set(color_codes=True)
+
+def vis_data_dist_ratio(channels = 1):
+    images22, _ = extract_data_ratio(data_path, channels, 22)
+    test_img22, _ = extract_data_ratio(test_path, channels, 22)
+    images22 =  np.hstack([images22.ravel(), test_img22.ravel()])
+
+    images30, _ = extract_data_ratio(data_path, channels, 30)
+    test_img30, _ = extract_data_ratio(test_path, channels, 30)
+    images30 =  np.hstack([images30.ravel(), test_img30.ravel()])
+
+    images45, _ = extract_data_ratio(data_path, channels, 45)
+    test_img45, _ = extract_data_ratio(test_path, channels, 45)
+    images45 =  np.hstack([images45.ravel(), images45.ravel()])
+
+    means = np.zeros(3)
+    stds = np.zeros(3)
+    textstrs = [' ', ' ', ' ']
+    name = ['22% pores', '30% pores', '45% pores']
+    col = ['b', 'r', 'g']
+    ratios = [images22, images30, images45]
+    support = np.linspace(0, 255, 1000)
+
+    fig,axes=plt.subplots(1,3,figsize=(24,13.5))
+    fig.suptitle('Distribution of intensities', fontsize=18, fontweight="bold")
+    props = dict(boxstyle='round', facecolor='silver', alpha=0.5)
+
+    for i in range(len(ratios)):
+        means[i] = ratios[i].mean()
+        stds[i] = ratios[i].std()
+        textstrs[i] = '\n'.join((
+            r'$\mu=%.2f$' % (means[i], ),
+            r'$\sigma=%.2f$' % (stds[i], )))
+        sns.distplot(ratios[i], kde=True, ax=axes[i], color=col[i])
+        axes[i].set(xlabel='Intensity', ylabel='Density', title = name[i])
+        axes[i].text(0.05, 0.95, textstrs[i], transform=axes[i].transAxes, fontsize=14,
+            verticalalignment='top', bbox=props)
+
+    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+
+    plt.show()
+
+if __name__ == '__main__':
+    vis_data_dist_ratio()
