8179785a2cfc8a06155d1f35bc7f96b13b9768bb
williamjvisuri <williamvisuri@gmail.com>
2020-05-08 10:18:00 +0200

    index print

--- visualization/exit_print.py
+++ visualization/exit_print.py
@@ -13,13 +13,12 @@ def exit_print(list_of_dicts, gpu, network, channels):
         sys.exit()
     header = list_of_dicts[0].keys()
     rows =  [list(x.values()) for x in sorted(list_of_dicts, key = lambda m: m['Mean IoU'],reverse=True)[:max_lines]]
-
     #print(rows)
 
     test = pd.DataFrame(data = rows, columns = header)
     test.to_csv('./results/{}/{}_{}_channels.csv'.format(gpu, network, channels), index_label = 'Index')
 
     print('\n\n')
-    print(tabulate.tabulate(rows, header, tablefmt="fancy_grid", stralign="right", floatfmt=(".3f", "2d", ".2e", "d", "d", ".3f", ".3f")))
+    print(tabulate.tabulate(test, headers='keys', tablefmt="fancy_grid", stralign="right", floatfmt=("d", ".3f", "2d", ".2e", "d", "d", ".3f", ".3f")))
     print('\n\n')
     sys.exit()
