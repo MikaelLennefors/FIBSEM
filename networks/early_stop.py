9c554e0636401d63d8a6b0a44edcdd93558da42f
MikaelLennefors <miklen@student.chalmers.se>
2020-06-13 09:56:41 +0100

    comments

--- networks/early_stop.py
+++ networks/early_stop.py
@@ -4,7 +4,8 @@ from tensorflow import keras
 
 
 class EarlyStoppingBaseline(tf.keras.callbacks.Callback):
-  """Stop training when the loss is at its min, i.e. the loss stops decreasing.
+  """
+  Stop training if accuracy has not overcome 72 %.
 
   Arguments:
       patience: Number of epochs to wait after min has been hit. After this
@@ -25,33 +26,27 @@ class EarlyStoppingBaseline(tf.keras.callbacks.Callback):
     # The epoch the training stops at.
     self.stopped_epoch = 0
     # Initialize the best as infinity.
-    # self.best = np.Inf
     self.best = 0.72
 
   def on_epoch_end(self, epoch, logs=None):
     current = logs.get('val_accuracy')
     if current != None:
         if np.greater(current, self.best):
-          # self.best = 0.5
-          # self.patience = 1
           self.wait = 0
-          # Record the best weights if current results is better (greater).
-          # self.best_weights = self.model.get_weights()
-          # self.best = 0.6
         else:
           self.wait += 1
           if self.wait >= self.patience:
             self.stopped_epoch = epoch
             self.model.stop_training = True
-            # print('Restoring model weights from the end of the best epoch.')
-            # self.model.set_weights(self.best_weights)
 
   # def on_train_end(self, logs=None):
   #   if self.stopped_epoch > 0:
   #     print('Epoch %05d: early stopping baseline'  % (self.stopped_epoch + 1))
 
 class EarlyStoppingDelta(tf.keras.callbacks.Callback):
-  """Stop training when the loss is at its min, i.e. the loss stops decreasing.
+  """
+  Stop training when the does not increas more than delta after patience number
+  of epochs.
 
   Arguments:
       patience: Number of epochs to wait after min has been hit. After this
